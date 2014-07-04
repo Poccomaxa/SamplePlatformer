906882bac29aba0a3653586a711dbf6911e35300
Poccomaxa <poc.leb@mail.ru>
2014-07-04 14:21:02 +0400

    Prefabs, physics, camera
    Wrapped player and level in prefabs, added camera following player,
    added animation of box collider in jump.

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+	public GameObject player;
+	public float snapDistance;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 distance = (player.transform.position - this.transform.position);
+		if (distance.magnitude > snapDistance) {
+			distance = distance * ((distance.magnitude - snapDistance) / distance.magnitude);
+			transform.position = transform.position + (Vector3)distance;
+		}
+	
+	}
+}
