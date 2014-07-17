b308673b9ad81bd548a3f0f3ca7dff2d53d2a224
Poccomaxa <poc.leb@mail.ru>
2014-07-17 23:58:10 +0400

    Remove levels from game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 	public static LevelController currentLevel;
+	public LevelController[] levels;
+	public GameObject player;
 
 	// Use this for initialization
 	void Start () {
-	
+		currentLevel = (LevelController)Instantiate(levels[0]);
+		GameObject.FindGameObjectsWithTag("MainCamera")[0].GetComponent<FollowPlayer>().player = (GameObject)Instantiate(player);
 	}
 	
 	// Update is called once per frame
